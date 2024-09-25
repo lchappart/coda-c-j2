747362068dc87980b799d5df6e8f3543c389bae6
lchappart <leo.chappart@coda-student.school>
2024-09-25 14:01:11 +0200

    Exercice 4

---
+++ ex4/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int var1; 
+	int var2;
+	int resultat;
+	var1=3;
+	var2=4;
+	resultat = var1*var2;
+	printf("%d x %d = %d \n",var1 , var2, resultat );
+	return 0;
+}
